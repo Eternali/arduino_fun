4c54f8c6b1ee6d52696c25361aed5876e458d6bc
conrad <conrad.heidebrecht@gmail.com>
2016-12-12 22:54:29 -0500

    small changes to hbridge

--- hbridge/hbridge.ino
+++ hbridge/hbridge.ino
@@ -3,15 +3,15 @@
 
 // declare pins being used
 const int enPin1 = 11;          // enable pin for motor 1
-const int enPin2 = 12;          // enable pin for motor 2
+const int enPin2 = 6;          // enable pin for motor 2
 const int inPin1 = 10;          // input pin 1 for motor 1
 const int inPin2 = 9;           // input pin 2 for motor 1
-const int inPin3 = 8;           // input pin 1 for motor 2
-const int inPin4 = 7;           // input pin 2 for motor 2
-const int switchPin1 = 6;       // switch pin for motor 1
-const int switchPin2 = 5;       // switch pin for motor 2
-const int potPin1 = 0;          // potential (analog) pin for motor 1
-const int potPin2 = 1;          // potential (analog) pin for motor 2
+const int inPin3 = 5;           // input pin 1 for motor 2
+const int inPin4 = 3;           // input pin 2 for motor 2
+const int switchPin1 = 4;       // switch pin for motor 1
+const int switchPin2 = 2;       // switch pin for motor 2
+//const int potPin1 = 0;          // potential (analog) pin for motor 1
+//const int potPin2 = 1;          // potential (analog) pin for motor 2
 
 void setup () {
   // set pin modes
@@ -32,14 +32,14 @@ void loop () {
   
   //// Motor 1
   // read desired speed from potentiometer1 (divide by 4 because analogRead is 0-1024, h-bridge requires 0-255)
-  int speed1 = analogRead(potPin1) / 4;
+  int speed1 = 100; //analogRead(potPin1) / 4;
   // Reverse the direction of motor using switchpin1
   boolean reverse1 = digitalRead(switchPin1);
   // set the speed of motor 1
   setMotor(speed1, reverse1, 1);
 
   //// Motor 2
-  int speed2 = analogRead(potPin2) / 4;
+  int speed2 = 100; //analogRead(potPin2) / 4;
   boolean reverse2 = digitalRead(switchPin2);
   setMotor(speed2, reverse2, 2);
   
