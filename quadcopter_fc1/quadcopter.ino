dc812507d19df0e8e1005ca84e84d34661fff772
conrad <conrad.heidebrecht@gmail.com>
2016-12-14 22:41:02 -0500

    added start of quadfc

---
+++ quadcopter_fc1/quadcopter.ino
@@ -0,0 +1,22 @@
+// Arduino flight controller, for Quadcopters
+// Designed for the Arduino Leonardo processor on the nanowii flight controller
+// 
+// Author: Conrad Heidebrecht
+// Licenced under the GNU Public Licence
+
+#include <Math.h>
+#include <Servo.h>
+#include <Wire.h>
+
+//
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ quadcopter_fc1/quadcopter_fc1.ino
@@ -0,0 +1,40 @@
+// Arduino flight controller, for Quadcopters
+// Designed for the Arduino Leonardo processor on the nanowii flight controller
+// 
+// Author: Conrad Heidebrecht
+// Licenced under the GNU Public Licence
+
+#include <Math.h>
+#include <Servo.h>
+#include <Wire.h>
+
+//// initial variables
+
+// angle variables for gyro
+float angleX, angleY, angleZ = 0.0;
+
+// RX signals
+int throttle = THROTTLE_RMIN;
+volatile int rx_values[4];      // ROLL, PITCH, THROTTLE, YAW
+
+// PID variables
+double pid_roll_in,  pid_roll_out,  pid_roll_setp =  0;
+double pid_pitch_in, pid_pitch_out, pid_pitch_setp = 0;
+double pid_yaw_in,   pid_yaw_in,    pid_yaw_setp =   0;
+
+// motors
+int m0, m1, m2, m3;             // FL, FR, BR, BL (F: FRONT, B: BACK, R: RIGHT, L: LEFT)
+
+// track loop time
+unsigned long prev_time = 0;
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
